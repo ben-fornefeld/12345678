0294798484cead869f3cebb770f296dd4347c287
Ben Fornefeld <50748440+ben-fornefeld@users.noreply.github.com>
2020-10-16 13:07:13 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-	import 'package:APPNAME/features/presentation/ui/screens/main_page.dart';
+	import 'package:$APPNAME$/features/presentation/ui/screens/main_page.dart';
 	
 	void main() {
 		runApp(App());
