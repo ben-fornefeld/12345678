04ad085e73b9fc935c1a91ee65208d24d19eb516
Ben Fornefeld <ben.fornefeld@gmail.com>
2020-10-20 10:22:00 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-	import 'package:$APPNAME$/features/presentation/ui/screens/main_page.dart';
-	
-	void main() {
-		runApp(App());
-	}
-	
-	class App extends StatelessWidget {
-		@override
-		Widget build(BuildContext context) {
-			return MaterialApp(
-				title: 'Flutter App',
-				theme: ThemeData(),
-				home: MainPage(),                
-				debugShowCheckedModeBanner: false,
-			);
-		}
-	}
+import 'package:$APPNAME$/features/presentation/ui/screens/main_page.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(),
+      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
